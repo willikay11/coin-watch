e16fe5334d1084ce1b177083691d34ee47e06b2d
willikay11 <willikay11@gmail.com>
2017-09-18 18:55:30 +0300

    Linked with coin watch server

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  coins: any;
 
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.http.get('/coin-watch-server/api/coins')
+        .map(res => res.json())
+        .subscribe(data => {
+      this.coins = data
+    });
   }
 
 }
