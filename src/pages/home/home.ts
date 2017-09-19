a7fe8f6bd6512a980eeb3e9584791ac10e75e584
willikay11 <willikay11@gmail.com>
2017-09-19 09:21:52 +0300

    Added loading states

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
@@ -10,14 +10,40 @@ import 'rxjs/add/operator/map';
 export class HomePage {
 
   coins: any;
+  nextPage: any;
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController, public http: Http, public loadingCtrl: LoadingController) {
+
+    let loading = this.loadingCtrl.create({
+      spinner: 'crescent',
+      content: ' <div class="custom-spinner-container">' +
+      ' <div class="custom-spinner-box">' +
+      '<span>Loading info ...</span>'+
+      '</div>' +
+      ' </div>',
+    });
+
+    loading.present();
 
     this.http.get('/coin-watch-server/api/coins')
         .map(res => res.json())
         .subscribe(data => {
-      this.coins = data
+          loading.dismiss();
+        this.coins = data.data;
+        this.nextPage = data.next_page_url;
     });
+  }
+
+  doInfinite(infiniteScroll) {
+    this.http.get('/coin-watch-server/api/coins' + this.nextPage)
+        .map(res => res.json())
+        .subscribe(data => {
+          this.nextPage = data.next_page_url;
+            console.log(data.data);
+            console.log(this.nextPage);
+        });
+
+    infiniteScroll.complete();
   }
 
 }
